26ffc4a78f098a57d26f51a3f3f2f0751df7d210
Pipetto-crypto <pipetto-crypto@gmail.com>
2026-08-22 10:01:47 +0200

    Revert "import winlator xinput and dinput dlls"
    This reverts commit c38fbe315ced66b0f21f4c1a7353065b3e20d7a1.

--- dlls/dinput/dinput.c
+++ dlls/dinput/dinput.c
@@ -282,7 +282,6 @@ static HRESULT WINAPI dinput7_CreateDeviceEx( IDirectInput7W *iface, const GUID 
 
     if (IsEqualGUID( &GUID_SysKeyboard, guid )) hr = keyboard_create_device( impl, guid, &device );
     else if (IsEqualGUID( &GUID_SysMouse, guid )) hr = mouse_create_device( impl, guid, &device );
-    else if (IsEqualGUID( &GUID_Joystick, guid )) hr = gamepad_create_device( impl, guid, &device );
     else hr = hid_joystick_create_device( impl, guid, &device );
 
     if (FAILED(hr)) return hr;
@@ -379,9 +378,6 @@ static HRESULT WINAPI dinput8_EnumDevices( IDirectInput8W *iface, DWORD type, LP
             if (hr == DI_OK && try_enum_device( device_type, callback, &instance, context, flags ) == DIENUM_STOP)
                 return DI_OK;
         } while (SUCCEEDED(hr));
-        hr = gamepad_enum_device( type, flags, &instance, impl->dwVersion );
-        if (hr == DI_OK && try_enum_device( device_type, callback, &instance, context, flags ) == DIENUM_STOP)
-            return DI_OK;
     }
 
     return DI_OK;
@@ -894,8 +890,6 @@ HRESULT WINAPI DllGetClassObject( REFCLSID clsid, REFIID iid, void **out )
     return CLASS_E_CLASSNOTAVAILABLE;
 }
 
-#if DIRECTINPUT_VERSION == 0x0700
-
 HRESULT WINAPI DirectInputCreateEx( HINSTANCE hinst, DWORD version, REFIID iid, void **out, IUnknown *outer )
 {
     IUnknown *unknown;
@@ -925,18 +919,6 @@ HRESULT WINAPI DirectInputCreateEx( HINSTANCE hinst, DWORD version, REFIID iid, 
 
     return DI_OK;
 }
-
-HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateA( HINSTANCE hinst, DWORD version, IDirectInputA **out, IUnknown *outer )
-{
-    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7A, (void **)out, outer );
-}
-
-HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateW( HINSTANCE hinst, DWORD version, IDirectInputW **out, IUnknown *outer )
-{
-    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7W, (void **)out, outer );
-}
-
-#else
 
 HRESULT WINAPI DECLSPEC_HOTPATCH DirectInput8Create( HINSTANCE hinst, DWORD version, REFIID iid, void **out, IUnknown *outer )
 {
@@ -970,4 +952,12 @@ HRESULT WINAPI DECLSPEC_HOTPATCH DirectInput8Create( HINSTANCE hinst, DWORD vers
     return S_OK;
 }
 
-#endif
+HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateA( HINSTANCE hinst, DWORD version, IDirectInputA **out, IUnknown *outer )
+{
+    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7A, (void **)out, outer );
+}
+
+HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateW( HINSTANCE hinst, DWORD version, IDirectInputW **out, IUnknown *outer )
+{
+    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7W, (void **)out, outer );
+}
--- dlls/dinput/tests/driver_hid.c
+++ dlls/dinput/tests/driver_hid.c
@@ -57,8 +57,6 @@ static void check_device( DEVICE_OBJECT *device )
 
     ok( device == impl->expect_hid_fdo, "got device %p\n", device );
     ok( device->DriverObject == expect_driver, "got DriverObject %p\n", device->DriverObject );
-    if (!device->NextDevice) ok( device == impl->expect_hid_fdo, "got device %p\n", device );
-    else ok( device->NextDevice == impl->expect_hid_fdo, "got NextDevice %p\n", device->NextDevice );
     ok( !device->AttachedDevice, "got AttachedDevice %p\n", device->AttachedDevice );
 
     ok( ext->MiniDeviceExtension == impl->expect_hid_ext, "got MiniDeviceExtension %p\n", ext->MiniDeviceExtension );
@@ -188,7 +186,6 @@ static NTSTATUS WINAPI driver_ioctl( DEVICE_OBJECT *device, IRP *irp )
     case IOCTL_WINETEST_HID_WAIT_EXPECT:
     case IOCTL_WINETEST_HID_SEND_INPUT:
     case IOCTL_WINETEST_HID_SET_CONTEXT:
-    case IOCTL_WINETEST_HID_WAIT_INPUT:
         IoSkipCurrentIrpStackLocation( irp );
         return IoCallDriver( ext->PhysicalDeviceObject, irp );
 
